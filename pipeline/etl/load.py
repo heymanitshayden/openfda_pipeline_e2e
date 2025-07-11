2f61ab7e310ba59649e6172c38b1969040d2f70c
hayden thomas <hayden.thomas@l7informatics.com>
2025-07-11 02:34:06 -0500

    final commit for loading adverse_events data; creating bridges between drugs, reactions, and adverse_events

--- pipeline/etl/load.py
+++ pipeline/etl/load.py
@@ -11,6 +11,9 @@ DB_CONFIG = {
     "host": os.getenv("POSTGRES_HOST"),
     "port": os.getenv("POSTGRES_PORT")
 }
+if not all(DB_CONFIG.values()):
+    raise EnvironmentError("Missing one or more required database environment variables.")
+
 
 def _get_or_create_row_id(cur, table, value):
     cur.execute(f"SELECT id FROM {table} WHERE name = %s", (value,))
@@ -22,7 +25,7 @@ def _get_or_create_row_id(cur, table, value):
 
 def _insert_adverse_event(cur, event):
     sql = """
-        INSERT INTO adverse_events (saftey_report_id, receive_date, serious, seriousness_death, seriousness_hospitalization)
+        INSERT INTO adverse_events (safety_report_id, receive_date, serious, seriousness_death, seriousness_hospitalization)
         VALUES (%s, %s, %s, %s, %s)
         ON CONFLICT (safety_report_id) DO NOTHING
     """
@@ -34,9 +37,26 @@ def _insert_adverse_event(cur, event):
         event["seriousnesshospitalization"],
     ))
 
-def _link_event_to_dimensions(cur, event):
-    return None
+def _link_event_to_dimensions(cur, safety_report_id, drugs, reactions):
+    EVENT_DRUG_SQL = """
+        INSERT INTO event_drug(safety_report_id, drug_id)
+        VALUES (%s, %s)
+        ON CONFLICT DO NOTHING
+    """
+    EVENT_REACTIONS_SQL = """
+        INSERT INTO event_reactions(safety_report_id, reaction_id)
+        VALUES (%s, %s)
+        ON CONFLICT DO NOTHING
+    """
+    
+    for drug in drugs:
+        drug_id = _get_or_create_row_id(cur, "drugs", drug)
+        cur.execute(EVENT_DRUG_SQL, (safety_report_id, drug_id))
 
+    for reaction in reactions:
+        reaction_id = _get_or_create_row_id(cur, "reactions", reaction)
+        cur.execute(EVENT_REACTIONS_SQL, (safety_report_id, reaction_id))
+    
 def load_new_records(records):
     conn = psycopg2.connect(**DB_CONFIG)
     try:
